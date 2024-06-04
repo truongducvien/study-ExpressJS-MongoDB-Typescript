caa58a66496dbff7ffa983751666f0a5709007be
vien.truong <truongducvien97@gmail.com>
2024-06-04 20:44:56 +0700

    Feat: Configure pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'study-nodejs-mongodb',
+      script: './src/app.ts',
+      watch: ['server'],
+      watch_delay: 1000,
+      ignore_watch: ['*.log'],
+
+      // Logs
+      time: true,
+
+      // Control flow:
+      autorestart: true
+    }
+  ]
+};
