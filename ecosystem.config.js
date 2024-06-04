a918accf0f734f1bb741a595a682ceee6f571248
vien.truong <truongducvien97@gmail.com>
2024-06-04 21:13:36 +0700

    Feat: Install bun

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'study-nodejs-mongodb',
-      script: './src/app.ts',
+      script: 'src/app.ts',
       watch: ['server'],
       watch_delay: 1000,
       ignore_watch: ['*.log'],
